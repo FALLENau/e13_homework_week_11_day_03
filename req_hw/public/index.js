8e906a65ab1b4ba21dc2bb090020c0c6df4ae470
FALLENau <reece.c.jones@gmail.com>
2017-08-02 21:28:22 +0100

    refactor

--- req_hw/public/index.js
+++ req_hw/public/index.js
@@ -4,3 +4,42 @@ var makeRequest = function(url, callback) {
   request.open('GET', url)
   request.send()
 }
+
+var beers
+
+var requestComplete = function() {
+  if (this.status !== 200) return;
+
+  var jsonString = this.responseText;
+  beers = JSON.parse(jsonString);
+  populateList(beers)
+}
+
+var populateList = function() {
+
+  var ul = document.querySelector("#info-field");
+  // console.log(beers.name)
+  beers.forEach(function(beer) {
+    var li = document.createElement("li");
+    li.classList.add("beer-item");
+    li.innerText = beer.name;
+    // beer.value = regionCountries.indexOf(country);
+
+    ul.appendChild(li);
+  });
+}
+
+var app = function () {
+  // var jsonString = localStorage.getItem("favCountry");
+  // var favCountry = JSON.parse(jsonString) || [];
+  // recreateInfo(favCountry);
+
+  var url = "https://api.punkapi.com/v2/beers";
+  makeRequest(url, requestComplete);
+
+  var beerSelect = document.querySelector("#beer-list");
+  // beerSelect.addEventListener("change", beerInfo);
+}
+
+
+window.addEventListener('load', app);
